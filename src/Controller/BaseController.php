11185d578157d28b2d5a3da9ff95b13e1d8d0538
ameta <ameta@epoka.edu.al>
2022-09-09 16:27:45 +0200

    create admin controller

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @method User getUser()
+ */
+
+abstract class BaseController extends AbstractController
+{
+
+
+}
