187edb28d3eb9e24e8b6fe753af427ddec54ab74
AlbanXhepi21 <axhepi21@gmail.com>
2022-09-19 23:49:59 +0200

    finished the slot bar, fully working

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -100,6 +100,50 @@ class UserController extends BaseController
         $reservationsA= $allReservations->findBy(['reservationStatus'=>'approved', 'date'=>$date]);
         $reservations=$this->getUpdatedStatus($reservationsA);
 
+        $userReservation= $allReservations->findBy(['user'=>$this->getUser()]);
+
+
+
+        $newStatus= $request->query->get('status_array' ,null);
+
+        $intArray = array_map(
+            function($value) { return (int)$value; },
+            explode(',',$newStatus)
+        );
+        $repeat=true;
+
+        foreach ($userReservation as $singleReservation)
+            {
+
+                if($singleReservation->getStatus()===$intArray)
+                {
+                    $repeat=false;
+                }
+            }
+
+            if($newStatus != null && explode(',',$newStatus) != [1,1,1,1,1,1,1]  && $repeat)
+            {
+
+
+
+                $newReservation = new Reservation();
+
+
+                $newReservation->setStatus($intArray);
+                $newReservation->setDate($date);
+                $newReservation->setRoom($room);
+                $newReservation->setUser($this->getUser());
+
+
+
+                $entityManager->persist($newReservation);
+                $entityManager->flush();
+            }
+
+
+
+
+
 
 
 
@@ -117,6 +161,8 @@ class UserController extends BaseController
 
 
     }
+
+
 
     #[Route('/states')]
     public function userState( ):Response
